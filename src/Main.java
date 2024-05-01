7fa4909dd0898428f3476bac0ecfc6f2e9aa1712
Lucas da Silva <lucas2005sf@gmail.com>
2024-04-30 21:40:29 -0300

    media-notas

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double nota1 = 9.5;
+        double nota2 = 6.5;
+        double media = (nota1 + nota2)/2;
+        System.out.println("A média das notas é: " + media);
+    }
+}
